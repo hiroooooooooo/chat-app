d4975c953cf39f4d0efc4e248b236c5335f15031
hiroooooooooo <piro.a1013@gmail.com>
2021-05-31 00:41:00 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
   root to: "messages#index"
+  # ユーザー編集に必要なルーティングは、editとupdateのみ
+  resources :users, only: [:edit, :update]
 end
