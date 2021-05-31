b50fddc76debe7703e5e4a88942536c456c42939
hiroooooooooo <piro.a1013@gmail.com>
2021-05-31 15:23:02 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: "messages#index"
   # ユーザー編集に必要なルーティングは、editとupdateのみ
   resources :users, only: [:edit, :update]
+  # 新規チャットルームの作成に必要なルーティングは、newとcreateのみ
+  resources :rooms, only: [:new, :create]
 end
