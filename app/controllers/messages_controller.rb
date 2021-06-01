f7305ec25e7726b5188fb05ec803b8f92d6d29ef
hiroooooooooo <piro.a1013@gmail.com>
2021-06-01 18:33:46 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -13,6 +14,8 @@ class MessagesController < ApplicationController
       redirect_to room_messages_path(@room)
       # redirect_to /rooms/#{@room.id}/messages ←多分こんな感じ
     else
+      # @messagesがないとindexを構成する素材が足りないので、ここで記述する
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
